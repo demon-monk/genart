78bab1b596f0202b9a09bfd1582de1305e6df085
demon-monk <antdude@126.com>
2019-06-03 23:16:42 +0800

    feat: use Gaussiab random distribution

--- sketch.js
+++ sketch.js
@@ -12,25 +12,28 @@ const sketch = () => {
   const createGrid = dimension =>
     Array.from({ length: dimension }, (item, idx) => idx)
       .map(x =>
-        Array.from({ length: dimension }, (item, idx) => idx).map(y => [
-          dimension <= 1 ? 0.5 : x / (dimension - 1),
-          dimension <= 1 ? 0.5 : y / (dimension - 1)
-        ])
+        Array.from({ length: dimension }, (item, idx) => idx).map(y => ({
+          position: [
+            dimension <= 1 ? 0.5 : x / (dimension - 1),
+            dimension <= 1 ? 0.5 : y / (dimension - 1)
+          ],
+          radius: Math.max(0, 0.005 + random.gaussian() * 0.02)
+        }))
       )
       .flat();
   random.setSeed(512);
-  const points = createGrid(100).filter(() => random.value() > 0.6);
+  const points = createGrid(30).filter(() => random.value() > 0.5);
   const margin = 300;
   console.log(points);
   return ({ context, width, height }) => {
     context.fillStyle = "wheat";
-    points.forEach(([u, v]) => {
+    points.forEach(({ position: [u, v], radius }) => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
       context.beginPath();
-      context.arc(x, y, 10, 0, Math.PI * 2, false);
-      context.lineWidth = 10;
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = "lightblue";
+      context.fill();
     });
   };
 };
