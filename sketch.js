e099cf3e8475b50a8758701bad6fe8e306ea5a72
demon-monk <antdude@126.com>
2019-06-03 22:39:04 +0800

    feat: add basic grid

--- sketch.js
+++ sketch.js
@@ -2,20 +2,32 @@ const canvasSketch = require("canvas-sketch");
 
 const settings = {
   dimensions: "A4",
-  orientation: 'landscape',
+  // orientation: "landscape",
   pixelsPerInch: 300 // for print, 72 for display on digital screens
 };
 
 const sketch = () => {
+  const createGrid = dimension =>
+    Array.from({ length: dimension }, (item, idx) => idx)
+      .map(x =>
+        Array.from({ length: dimension }, (item, idx) => idx).map(y => [
+          dimension <= 1 ? 0.5 : x / (dimension - 1),
+          dimension <= 1 ? 0.5 : y / (dimension - 1)
+        ])
+      )
+      .flat();
+  const points = createGrid(5);
+  console.log(points);
   return ({ context, width, height }) => {
     context.fillStyle = "wheat";
-    context.fillRect(0, 0, width, height);
-    context.fillStyle = "orange";
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
-    context.fill();
-    context.lineWidth = width * 0.05;
-    context.strokeStyle = "lightblue";
-    context.stroke();
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
+      context.beginPath();
+      context.arc(x, y, 200, 0, Math.PI * 2, false);
+      context.lineWidth = 50;
+      context.stroke();
+    });
   };
 };
 
